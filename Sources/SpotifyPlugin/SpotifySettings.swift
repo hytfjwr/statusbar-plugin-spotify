5007cf61aaaeb3a17b14997dd66a16026d5e0740
Hayato Fujiwara <133951156+hytfjwr@users.noreply.github.com>
2026-05-20 15:08:24 +0900

    feat: add animated waveform next to status bar icon (#11)
    Adds an animated waveform visualization next to the Spotify icon
    while playback is active, controlled by a new "Show waveform"
    toggle in settings (enabled by default).

--- Sources/SpotifyPlugin/SpotifySettings.swift
+++ Sources/SpotifyPlugin/SpotifySettings.swift
@@ -5,6 +5,7 @@ import Foundation
 final class SpotifySettings {
     private static let alwaysShowIconKey = "com.statusbar.spotify.alwaysShowIcon"
     private static let showTrackNameKey = "com.statusbar.spotify.showTrackName"
+    private static let showWaveformKey = "com.statusbar.spotify.showWaveform"
     private static let artworkColorEnabledKey = "com.statusbar.spotify.artworkColorEnabled"
     private static let artworkColorOpacityKey = "com.statusbar.spotify.artworkColorOpacity"
 
@@ -14,6 +15,10 @@ final class SpotifySettings {
 
     var showTrackName: Bool {
         didSet { UserDefaults.standard.set(showTrackName, forKey: Self.showTrackNameKey) }
+    }
+
+    var showWaveform: Bool {
+        didSet { UserDefaults.standard.set(showWaveform, forKey: Self.showWaveformKey) }
     }
 
     var artworkColorEnabled: Bool {
@@ -26,11 +31,13 @@ final class SpotifySettings {
 
     init() {
         UserDefaults.standard.register(defaults: [
+            Self.showWaveformKey: true,
             Self.artworkColorEnabledKey: true,
             Self.artworkColorOpacityKey: 0.35,
         ])
         alwaysShowIcon = UserDefaults.standard.bool(forKey: Self.alwaysShowIconKey)
         showTrackName = UserDefaults.standard.bool(forKey: Self.showTrackNameKey)
+        showWaveform = UserDefaults.standard.bool(forKey: Self.showWaveformKey)
         artworkColorEnabled = UserDefaults.standard.bool(forKey: Self.artworkColorEnabledKey)
         artworkColorOpacity = UserDefaults.standard.double(forKey: Self.artworkColorOpacityKey)
     }
